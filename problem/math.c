89eaddf344118aaf57d420f7d75d5d38dffbfd06
Harikrishnan0072004 <hariengineering42@gmail.com>
2025-08-03 10:14:17 +0530

    Add files via upload

---
+++ problem/math.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int base = 3;
+    int exponent = 5;
+    int result = pow(base , exponent);
+    printf("The pow :%lf\n",pow(8.0, 3));
+    printf("result %d",result);
+    return 0;
+}
